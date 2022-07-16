dba50e12f37f2f75db07a9977c0af81da6bf5221
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-07-16 08:04:50 +0300

    useEffect Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";    
 
 function App() {
-  useEffect(() => {
-    console.log("useEffect called");
-  });
+  const[count, setCount]= useState(0)
+  const[text, setText] = useState("")
+
+  useEffect(
+    // () => { console.log("useEffect called");},[]
+    () => {document.title = text;},[text]);
+  useEffect(
+    () => {setTimeout(() => setCount(0),5000);}, []
+  );
 
   console.log("Component rendering");
+  
+  // function handleClick (){
+  //   setCount((count) => count + 1)
+  // }
+  function handleChange(event){
+    setText(event.target.value)
 
-  return <button>Click Me</button>;
+  }
+
+
+  return (
+    <div>
+      {/* <button onClick={() => handleClick}> I've been clicked {count} times</button> */}
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input type="text" value={text} onChange = {handleChange} placeholder="Type away..."/>
+      <DogPics/>
+    </div>
+  )
+  
 }
 
 export default App;
